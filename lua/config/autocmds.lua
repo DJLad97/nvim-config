8a492807ce0238a77fba9d0828dd5db74ce0c7d6
Dan Scott <me@danjscott.dev>
2024-06-10 21:54:47 +0100

    auto indent for go files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,19 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- Auto commands for Go files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    -- Set tab width to 4 spaces
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    -- Use tabs instead of spaces
+    vim.opt_local.expandtab = false
+    -- Enable auto indentation
+    vim.opt_local.autoindent = true
+    vim.opt_local.smartindent = true
+  end
+})
